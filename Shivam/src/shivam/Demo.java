4c72a626f32678146b9d33838e661a14d8f6cd30
gshivammsg <gshivammsg1997@gmail.com>
2018-10-01 08:28:11 +0530

    sibling window change color

---
+++ Shivam/src/shivam/Demo.java
@@ -0,0 +1,30 @@
+package shivam;
+
+
+ public class Demo {
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+
+        CodeKampFrame frame1 = new CodeKampFrame();
+
+        CodeKampFrame frame2 = new CodeKampFrame();
+
+
+
+        frame1.setSibling(frame2);
+
+
+
+        frame1.setVisible(true);
+
+        frame2.setVisible(true);
+
+    }
+
+}
